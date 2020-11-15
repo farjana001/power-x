5e304195ca3837753a35e5083d4047f541d13281
farjana001 <nipa.farzana87@gmail.com>
2020-11-15 20:42:29 +0600

    done footer

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AboutUs from '../AboutUs/AboutUs';
 import ChooseUs from '../ChooseUs/ChooseUs';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Services from '../Services/Services';
 
@@ -11,6 +12,7 @@ const Home = () => {
             <Services />
             <AboutUs />
             <ChooseUs />
+            <Footer />
         </>
     );
 };
